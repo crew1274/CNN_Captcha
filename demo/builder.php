f0e08a965a60c76a037de88889a7091b27419988
crew1274 <crew1274@yahoo.com.tw>
2017-01-06 15:04:43 +0800

    update

---
+++ demo/builder.php
@@ -0,0 +1,16 @@
+<?php
+include(__DIR__.'/../CaptchaBuilderInterface.php');
+include(__DIR__.'/../PhraseBuilderInterface.php');
+include(__DIR__.'/../CaptchaBuilder.php');
+include(__DIR__.'/../PhraseBuilder.php');
+use Gregwar\Captcha\CaptchaBuilder;
+for ( $i=0 ; $i<10; $i++ )
+{
+$captcha = new CaptchaBuilder;
+$captcha->
+setBackgroundColor(255, 255,255)->
+setMaxBehindLines(0)->setMaxFrontLines(0)->setInterpolation(false)->setDistortion(false)->build();
+    $label=$captcha->getPhrase();
+    $captcha->save('temp/'.$label.'.jpg');
+echo $label.'<br>';
+}
